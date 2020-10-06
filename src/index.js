777855bbb274cc3779b07e3be97bca1e9931d76d
Lika <borisovalika2@mail.com>
2020-10-06 16:37:29 +0300

    feat: brackets function solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
+// jshint esversion:6
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let configBr = [];
+  let newStr = str;
+
+  for (let item of bracketsConfig) {
+    // конкатенируем скобки, что сделать одну str скобок внутри массива
+    configBr.push(item[0] + item[1]);
+  }
+  // console.log(configBr);
+
+  for (let i = 0; i <= str.length; i++) {
+    for (let k = 0; k <= configBr.length; k++) {
+      newStr = newStr.replace(configBr[k], '');
+    }
+    if (newStr.length === 0) {
+      return true;
+    }
+  }
+  return false;
+};
